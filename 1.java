b064ddbae58403fa461150beed1c02ceed18e115
Gopalagrawal-12 <gopal.agrawal_cs.aiml24@gla.ac.in>
2025-08-27 12:18:34 +0530

    Update 1.java

--- 1.java
+++ 1.java
@@ -2,7 +2,6 @@ class Book {
     String title;
     String author;
 
-    // Constructor
     Book(String title, String author) {
         this.title = title;
         this.author = author;
@@ -11,22 +10,14 @@ class Book {
 
 public class Main {
     public static void main(String[] args) {
-        // Step 1: Create an array of 3 Book references
         Book[] books = new Book[3];  
 
-        // Edge Case: If we print now without initialization
-        System.out.println("Before initialization:");
-        for (int i = 0; i < books.length; i++) {
-            System.out.println(books[i]);  // prints null, since references are not pointing to objects yet
-        }
+       
 
-        // Step 2: Initialize each element with a new Book object
-        books[0] = new Book("The Pragmatic Programmer", "Andrew Hunt & David Thomas");
-        books[1] = new Book("Effective Java", "Joshua Bloch");
-        books[2] = new Book("Thinking in Java", "Bruce Eckel");
+        books[0] = new Book("c++", "Andrew ");
+        books[1] = new Book("Java", "Peter");
+        books[2] = new Book("Gaming in python", "Bruce");
 
-        // Step 3: Print out the titles of all books
-        System.out.println("\nAfter initialization:");
         for (Book b : books) {
             System.out.println(b.title);
         }
